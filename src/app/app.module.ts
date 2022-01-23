030e76d5439eb1d9c37daaa10f8ed00ce331b870
SRBOOMC-PC\Admin <siripaakc@gmail.com>
2022-01-24 03:17:37 +0700

    Add content in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_components/header/header.component';
+import { HomeComponent } from './_pages/home/home.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
