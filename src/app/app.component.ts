9beea2d525d85f297d822492f94b11073bf53609
SRBOOMC-PC\Admin <siripaakc@gmail.com>
2022-01-24 13:46:08 +0700

    styling cover page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,7 @@ import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'siripaakchinnachot';
 
-  constructor(private fb: FacebookService){}
-
-  ngOnInit(): void {
-    // this.initFacebookService();
-  }
-
-  // private initFacebookService(): void {
-  //   const initParams: InitParams = { 
-  //     appId: '448222067012224',
-  //     autoLogAppEvents : true,
-  //     xfbml:true, 
-  //     version:'v3.2'
-  //   };
-  //   this.fb.init(initParams);
-  // }
-
-  // loginWithFacebook(): void {
- 
-  //   this.fb.login()
-  //     .then((response: LoginResponse) => console.log(response))
-  //     .catch((error: any) => console.error(error));
-  // }
 }
