f525f59432d6ba6df0e63b326640c878ec4aaa53
SRBOOMC-PC\Admin <siripaakc@gmail.com>
2022-04-21 00:56:01 +0700

    `Portfolio`

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Competency } from 'src/app/_model/Competency';
 import { Skill } from 'src/app/_model/Skill';
 
@@ -52,36 +53,97 @@ export class HomeComponent implements OnInit {
   navList: Array<string> = ['WHO AM I', 'EDUCATION'];
   selected: string = 'WHO AM I';
 
-  skills: Skill[] = [
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: [
+      '<i class="fa-solid fa-chevron-left"></i>',
+      '<i class="fa-solid fa-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 3,
+      },
+    },
+    nav: true,
+  };
+
+  portfolios: any = [
     {
       id: 1,
-      name: 'TypeScript',
-      percent: 85,
+      name: 'A1CE Mobile application',
+      category: 'application',
+      image: '../../../assets/images/A1CE.png',
+      link: '#',
     },
     {
       id: 2,
-      name: 'JavaScript',
-      percent: 85,
+      name: 'Alinea Bkk',
+      category: 'website',
+      image: '../../../assets/images/Alinea.png',
+      link: 'https://alineabkk.netlify.app',
     },
     {
       id: 3,
-      name: 'HTML',
-      percent: 80,
+      name: 'Intlab',
+      category: 'website',
+      image: '../../../assets/images/Intlab.png',
+      link: '#',
     },
     {
       id: 4,
-      name: 'CSS',
-      percent: 65,
+      name: 'Interactive Cozmo',
+      category: 'Internet Of Things',
+      image: '../../../assets/images/unavailable_image_placeholder.png',
+      link: 'https://www.youtube.com/watch?v=ZjOdVatHVoQ&list=PLa8wyslUO1luCs5ThOqLlsp9B1bERSKh6&index=22&ab_channel=TagunJivasitthikul',
     },
     {
       id: 5,
-      name: 'Python',
-      percent: 60,
+      name: 'Automatic adjust table',
+      category: 'Internet Of Things',
+      image: '../../../assets/images/unavailable_image_placeholder.png',
+      link: 'https://cie.kmitl.ac.th/news/cie-students-project-series-ep-3-smart-office-table/',
     },
     {
       id: 6,
-      name: 'SQL',
-      percent: 75,
+      name: 'Automatic check attendance',
+      category: 'Internet Of Things',
+      image: '../../../assets/images/unavailable_image_placeholder.png',
+      link: '#',
+    },
+    {
+      id: 7,
+      name: 'Learning Course website design',
+      category: 'Design',
+      image: '../../../assets/images/Course.png',
+      link: 'https://www.figma.com/file/hVagusrRNa0iLv3bckQ4Af/SiripaakChinnachot_61011334?node-id=0%3A1',
+    },
+    {
+      id: 8,
+      name: 'Smart piggy bank',
+      category: 'Internet Of Things',
+      image: '../../../assets/images/unavailable_image_placeholder.png',
+      link: 'https://www.youtube.com/watch?v=efmyejVVIIs&list=PLa8wyslUO1luCs5ThOqLlsp9B1bERSKh6&index=4&ab_channel=TagunJivasitthikul',
+    },
+    {
+      id: 9,
+      name: 'Zombie survival game',
+      category: 'Virtual Reality',
+      image: '../../../assets/images/unavailable_image_placeholder.png',
+      link: '#',
     },
   ];
   constructor() {}
@@ -104,8 +166,9 @@ export class HomeComponent implements OnInit {
     this.selected = nav;
   }
 
-  openModal(src: string) {
-    this.isShowModal = !this.isShowModal;
-    this.showImage = src;
+  onNavigate(link: string) {
+    if (link !== '#') {
+      window.open(link, '_blank');
+    }
   }
 }
